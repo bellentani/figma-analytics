41af2fafb16982763dba17bcfcc54dc3af7a2b14
Rafeal Sanches <fucazu@gmail.com>
2024-11-12 20:14:46 -0300

    commit log fileId

--- figmaService.js
+++ figmaService.js
@@ -2,16 +2,26 @@ const axios = require('axios');
 const { FIGMA_API_URL, FIGMA_ANALYTICS_URL, FIGMA_TOKEN } = require('./config');
 
 async function fetchFigmaFile(fileId) {
-    const response = await axios.get(`${FIGMA_API_URL}${fileId}`, {
-        headers: {
-            'X-Figma-Token': FIGMA_TOKEN
-        }
-    });
-    return response.data;
+  try {
+    console.log('fileId:', fileId);
+      const response = await axios.get(`${FIGMA_API_URL}${fileId}`, {
+          headers: {
+              'X-Figma-Token': FIGMA_TOKEN
+          }
+      });
+      return response.data;
+  } catch (error) {
+      console.error(`Error fetching Figma file ${fileId}:`, error.message);
+      if (error.response) {
+          console.error('Status:', error.response.status);
+          console.error('Data:', error.response.data);
+      }
+      throw error;
+  }
 }
 
 async function fetchFigmaAnalytics(libraryId) {
-    const response = await axios.get(`${FIGMA_ANALYTICS_URL}${libraryId}`, {
+    const response = await axios.get(`${FIGMA_ANALYTICS_URL}${libraryId}/components`, {
         headers: {
             'X-Figma-Token': FIGMA_TOKEN
         }
